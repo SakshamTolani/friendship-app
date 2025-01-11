ec36134f53c771de2567e06ee18488d481c0d8c9
SakshamTolani <sakshamtolani@gmail.com>
2025-01-11 21:08:55 +0530

    Boilder plate for backend

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const { MONGOURI } = require("./config/keys");
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(MONGOURI);
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+connectDB();
+require("./models/user");
+require("./models/post");
+
+app.use(express.json());
+app.use(require("./routes/auth"));
+app.use(require("./routes/post"));
+app.use(require("./routes/user"));
+
+if (process.env.NODE_ENV == "production") {
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+app.listen(PORT, () => {
+    console.log("Server has started on port " + PORT);
+})
